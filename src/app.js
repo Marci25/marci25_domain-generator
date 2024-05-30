757ac8225cd716d1cbce5f2e60d0b4495fc2b3e5
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-05-30 19:13:17 +0000

    Mensaje

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function generateDomains(pronouns, adjectives, nouns) {
+    let domains = [];
+
+    for (let p of pronouns) {
+      for (let a of adjectives) {
+        for (let n of nouns) {
+          domains.push(`${p}${a}${n}.com`);
+        }
+      }
+    }
+    return domains;
+  }
+  let domainsList = generateDomains(pronoun, adj, noun);
+  domainsList.forEach(
+    domain =>
+      (document.querySelector("#Dominios").innerHTML +=
+        "<li>" + domain + "</li>")
+  );
 };
